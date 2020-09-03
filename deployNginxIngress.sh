1d903f0daa062e310213b2af59a84062ea197869
fabstao <fsalaman@gmail.com>
2020-09-03 00:27:50 -0500

    Added support for RedHat based, fixing Calico issue, still pending

--- deployNginxIngress.sh
+++ deployNginxIngress.sh
@@ -2,15 +2,15 @@
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/baremetal/deploy.yaml
 echo "Wait for NGINX ingress pods to complete"
-sleep 5
+sleep 15 
 kubectl get pods -n ingress-nginx   -l app.kubernetes.io/name=ingress-nginx 
 echo
 echo "Wait for NGINX ingress pods to complete"
-sleep 5
+sleep 20 
 kubectl get pods -n ingress-nginx   -l app.kubernetes.io/name=ingress-nginx 
 echo
 echo "Wait for NGINX ingress pods to complete"
-sleep 5
+sleep 8 
 echo "Check NGINX ingress version"
 POD_NAMESPACE=ingress-nginx
 POD_NAME=$(kubectl get pods -n $POD_NAMESPACE -l app.kubernetes.io/name=ingress-nginx --field-selector=status.phase=Running -o jsonpath='{.items[0].metadata.name}')
