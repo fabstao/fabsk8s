d484818d346f8778757eca2cca4e835e1d144133
fabstao <fsalaman@gmail.com>
2020-09-04 00:58:09 -0500

    Finally got Fedora working

--- deployNginxIngress.sh
+++ deployNginxIngress.sh
@@ -6,11 +6,11 @@ sleep 15
 kubectl get pods -n ingress-nginx   -l app.kubernetes.io/name=ingress-nginx 
 echo
 echo "Wait for NGINX ingress pods to complete"
-sleep 20 
+sleep 15 
 kubectl get pods -n ingress-nginx   -l app.kubernetes.io/name=ingress-nginx 
 echo
 echo "Wait for NGINX ingress pods to complete"
-sleep 8 
+sleep 11 
 echo "Check NGINX ingress version"
 POD_NAMESPACE=ingress-nginx
 POD_NAME=$(kubectl get pods -n $POD_NAMESPACE -l app.kubernetes.io/name=ingress-nginx --field-selector=status.phase=Running -o jsonpath='{.items[0].metadata.name}')
