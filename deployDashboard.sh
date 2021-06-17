66b2dcf401aeaffea157a55730bbf673b23aac6d
fabstao <fabs@koalatechie.com>
2021-06-16 21:23:41 -0500

    Fixed timeouts on deploy*.sh scripts

--- deployDashboard.sh
+++ deployDashboard.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended.yaml
 kubectl apply -f dashboard-user.yaml
 kubectl apply -f admin-clusterrolebinding.yaml
+sleep 15
 kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}') | grep token\:
+sleep 15
+kubectl -n kubernetes-dashboard get pods
 
 echo "Run kubectl proxy"
 echo 
